e82b380391e77dafac136a37deaa81014c493ea6
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-06-28 01:00:37 -0400

    completed numbers class 6

---
+++ notes/6/index.php
@@ -0,0 +1,46 @@
+<?php
+
+    // whole number is integer/int
+    $radius = 25;
+    // this one is float or a double
+    $pi = 3.14;
+
+    // basic = -,+, / , *, ** (not ^)
+
+    // echo $pi * $radius**2;
+
+    //B I D M A S
+    // brackets Indeces(**) Division multi addition subtraction
+
+    // echo 2 * (4+9) / 3;
+
+    // increment and decrement
+    // echo $radius--;
+    // echo $radius++;
+    // echo $radius;
+
+    // shorthand
+    // $age = 20;
+    // $age = $age + 10;
+    // $age += 10;
+    // echo $age;
+
+    // number functions
+    // echo floor($pi);
+    // echo ceil($pi);
+    // inbuilt pi
+    // echo pi();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Learning</title>
+</head>
+<body>
+    
+</body>
+</html>
