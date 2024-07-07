0fe7a59dcf36490fd3ed87667cc9a2acbb50724a
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-07-07 14:12:28 -0400

    arrays started

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
 
-   
+// indexed array
+
+$peopleOne = ['shaun', 'crystal', 'ryu'];
+// echo $peopleOne[1];
+
+$peopleTwo = array('ken', 'shaun');
+// echo $peopleTwo[1];
+
+$ages = [20, 30, 40, 50];
+
+//print readable
+print_r ($ages);
+
+// associative arrays
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Learning</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
