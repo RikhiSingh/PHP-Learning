4ffce386b1d94ebad70f57412c7657355c41b2b5
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-07-09 22:07:48 -0400

    booleans and comparisons

---
+++ notes/10/index.php
@@ -0,0 +1,45 @@
+<?php
+
+// comparison booleans (true or false)
+// echo true;
+// for true its 1 where as for false nothing
+// as the value is converted to string; for false no string generated (cuz browswer doesnt know what true or false is so just empty strin "" like this is returned)
+
+// echo 5 < 10;
+// echo 5 == 10;
+// we use double equal as we comparing; single is used for assigning
+
+// true as s comes before y
+// echo 'shaun' > 'yoshi';
+// true as uppercase is less than small case 
+// echo 'shaun' > 'Shaun';
+
+// loose vs strict equal comparison
+
+// true
+// echo 5 == '5';
+
+// false as strict comparison compares data type as well
+// echo 5 === '5';
+
+echo true == '1';
+echo false == '';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
