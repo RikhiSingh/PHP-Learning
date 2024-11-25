19b55300d52d8a8ff9f86ed94a0e2aeb967bf028
Rikhi Singh <114336052+RikhiSingh@users.noreply.github.com>
2024-11-25 14:58:01 -0500

    get method

---
+++ notes/17 forms/index.php
@@ -0,0 +1,22 @@
+<!-- 
+    GET and POST
+
+    - Both methods can be used to send data to the server
+
+    - GET sends data in the URL
+    - POST sends the data in request header (hidden)
+-->
+
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include('templates/header.php'); ?>
+    <?php include('templates/footer.php'); ?>
+
+</body>
+</html>
